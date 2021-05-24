80b4f38dd92e0776f64d815dd0347116efe7cd8b
sql <68092911+sql69@users.noreply.github.com>
2021-05-24 23:03:07 +0200

    Delete colors.py

--- colors.py
+++
@@ -1,6 +0,0 @@
-PINK = "\x1b[38;5;217m"
-RESET = "\u001b[0m"
-RED = "\u001b[31;1m"
-
-if __name__ == "__main__":
-    __import__("os").system("echo You're supposed to run main.py !!! && pause >NUL")
